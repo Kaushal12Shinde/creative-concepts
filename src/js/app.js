6286c9c3a0efb0da28f0cdf673b4fe691aadb6d0
Kaushal12Shinde <kaushushinde12@gmail.com>
2024-08-20 23:33:25 +0530

    fix/ scroll on to view

--- src/js/app.js
+++ src/js/app.js
@@ -50,3 +50,13 @@ if (logoSlider) {
     },
   });
 }
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
